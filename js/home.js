ea2cd42add87240916627e8241b057e92467a877
Sadia0920 <sadiaafrin0920@gmail.com>
2024-09-22 14:47:25 +0600

    added a utilities

--- js/home.js
+++ js/home.js
@@ -4,12 +4,24 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
     const addPin = getInputFieldValueById ('add-pin-input');
     // const addMoney =parseFloat(document.getElementById('add-money-input').value);
     // const addPin =parseFloat(document.getElementById('add-pin-input').value);
+    if(isNaN(addMoney)){
+        alert('Failed to add a money');
+        return;
+    }
     if(addPin === 1234){
         const availableBalance =getTextFieldValueById('balance');
         // const availableBalance =parseFloat(document.getElementById('balance').innerText);
         const newBalance = availableBalance + addMoney;
         document.getElementById('balance').innerText = newBalance;
         // console.log(newBalance);
+        // add to transactions history
+        const div = document.createElement('div');
+        div.classList.add('bg-blue-300');
+        div.innerHTML = `
+        <h4 class="text-2xl font-bold">Add Money</h4>
+        <p> ${addMoney} Tk. Added New Balance: ${newBalance}</p>
+        `
+        document.getElementById('transactions-container').appendChild(div);
     }
     else{
         alert('Wrong phone number or pin');
@@ -22,12 +34,28 @@ document.getElementById('btn-cashout-money').addEventListener('click',function(e
     const cashOutPin =getInputFieldValueById('cash-out-pin-input');
     // const cashOutMoney =parseFloat(document.getElementById('cash-out-input').value);
     // const cashOutPin =parseFloat(document.getElementById('cash-out-pin-input').value);
+    if(isNaN(cashOutMoney)){
+        alert('Failed to cash out');
+        return;
+    }
     if(cashOutPin === 1234){
         const availableBalance =getTextFieldValueById('balance');
+        if(cashOutMoney > availableBalance){
+            alert("You don't have enough money");
+            return;
+        }
         // const availableBalance =parseFloat(document.getElementById('balance').innerText);
         const newBalance = availableBalance - cashOutMoney;
         document.getElementById('balance').innerText = newBalance;
         // console.log(newBalance);
+        // Transaction section
+        const div = document.createElement('div');
+        div.classList.add('bg-blue-500');
+        div.innerHTML = `
+        <h4 class="text-2xl font-bold">Cash Out Money</h4>
+        <p> ${cashOutMoney} Tk. withdraw New Balance: ${newBalance}</p>
+        `
+        document.getElementById('transactions-container').appendChild(div);
     }
     else{
         alert('Wrong phone number or pin');
